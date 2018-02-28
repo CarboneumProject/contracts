b9d6b724bb27ca5fcfa70cc992f727e98260e34d
artiya4u <artiya4u@gmail.com>
2018-02-28 15:08:17 +0700

    Update sale price.

--- contracts/CarboneumToken.sol
+++ contracts/CarboneumToken.sol
@@ -4,13 +4,9 @@ import 'zeppelin-solidity/contracts/token/ERC20/MintableToken.sol';
 import 'zeppelin-solidity/contracts/token/ERC20/BurnableToken.sol';
 
 contract CarboneumToken is MintableToken, BurnableToken {
-    string public name = "Carboneum Protocol Token";
+    string public name = "Carboneum";
     string public symbol = "C8";
     uint8 public decimals = 18;
-
-    uint256 public constant MAX_SUPPLY = 200 * (10 ** 6) * (10 ** uint256(decimals));
-
     function CarboneumToken() public {
-
     }
 }
--- migrations/2_deploy_contracts_private_placement.js
+++ migrations/2_deploy_contracts_private_placement.js
@@ -4,9 +4,9 @@ const CarboneumToken = artifacts.require("./CarboneumToken.sol");
 module.exports = function (deployer, network, accounts) {
     const startTime = new web3.BigNumber(new Date().getTime() / 1000); // Now
     const endTime = new web3.BigNumber(new Date(2018, 3, 22, 3, 8, 0, 0).getTime() / 1000); // Util Pre-Sale at 22 March 2018 @10:08 (GMT +7)
-    const rate = new web3.BigNumber('9615'); // ETH Price at 800 USD,
+    const rate = new web3.BigNumber('9344'); // ETH Price at 800 USD,
     const wallet = accounts[0];
-    const cap = web3.toWei(105600000, 'ether'); // Limit 88% of sale token in wei
+    const cap = web3.toWei(60000000, 'ether'); // Limit 50% of sale token in wei
     const goal = web3.toWei(1, 'ether'); // No goal
 
     if (network === "live") {
