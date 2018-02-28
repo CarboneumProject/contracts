7d71085721beb2b6e8bcb052bd58ad0a9f5b06a5
artiya4u <artiya4u@gmail.com>
2018-02-28 19:07:01 +0700

    Fix date to be integer.

--- migrations/2_deploy_contracts_private_placement.js
+++ migrations/2_deploy_contracts_private_placement.js
@@ -2,8 +2,8 @@ const CarboneumCrowdsale = artifacts.require("./CarboneumCrowdsale.sol");
 const CarboneumToken = artifacts.require("./CarboneumToken.sol");
 
 module.exports = function (deployer, network, accounts) {
-    const startTime = new web3.BigNumber(new Date().getTime() / 1000); // Now
-    const endTime = new web3.BigNumber(new Date(2018, 3, 22, 3, 8, 0, 0).getTime() / 1000); // Util Pre-Sale at 22 March 2018 @10:08 (GMT +7)
+    const startTime = new web3.BigNumber(Math.floor(new Date().getTime() / 1000)); // Now
+    const endTime = new web3.BigNumber(Math.floor(new Date(2018, 3, 22, 3, 8, 0, 0).getTime() / 1000)); // Util Pre-Sale at 22 March 2018 @10:08 (GMT +7)
     const rate = new web3.BigNumber('9344'); // ETH Price at 800 USD,
     const wallet = accounts[0];
     const cap = web3.toWei(60000000, 'ether'); // Limit 50% of sale token in wei
--- readme.md
+++ readme.md
@@ -8,11 +8,11 @@ $ truffle migrate
 
 ## Try on truffle console 
 ```
-account0 = web3.eth.accounts[0]
+account1 = web3.eth.accounts[1]
 CarboneumCrowdsale.deployed().then(inst => { crowdsale = inst })
 crowdsale.token().then(addr => { tokenAddress = addr } )
 tokenAddress
 caboneumTokenInstance = CarboneumToken.at(tokenAddress)
-CarboneumCrowdsale.deployed().then(inst => inst.sendTransaction({ from: account0, value: web3.toWei(5, "ether")}))
+CarboneumCrowdsale.deployed().then(inst => inst.sendTransaction({ from: account1, value: web3.toWei(5, "ether")}))
 caboneumTokenInstance.balanceOf(account1).then(balance => account1C8TokenBalance = balance.toString(10))
 ```
