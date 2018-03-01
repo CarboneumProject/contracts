5282cc19713c758e95cb6c0dc401ccc28aef3016
artiya4u <artiya4u@gmail.com>
2018-03-01 22:49:40 +0700

    Crowdsale is work now. Tried pack everything into a file for better debugging on Remix.

--- migrations/2_deploy_contracts_private_placement.js
+++ migrations/2_deploy_contracts_private_placement.js
@@ -1,20 +1,33 @@
-const CarboneumCrowdsale = artifacts.require("./CarboneumCrowdsale.sol");
-const CarboneumToken = artifacts.require("./CarboneumToken.sol");
+const CarboneumCrowdsale = artifacts.require("CarboneumCrowdsale");
+const CarboneumToken = artifacts.require("CarboneumToken");
+const RefundVault = artifacts.require("RefundVault");
+
+function ether(n) {
+    return new web3.BigNumber(web3.toWei(n, 'ether'));
+}
 
 module.exports = function (deployer, network, accounts) {
     const startTime = new web3.BigNumber(Math.floor(new Date().getTime() / 1000)); // Now
     const endTime = new web3.BigNumber(Math.floor(new Date(2018, 3, 22, 3, 8, 0, 0).getTime() / 1000)); // Util Pre-Sale at 22 March 2018 @10:08 (GMT +7)
-    const rate = new web3.BigNumber('9344'); // ETH Price at 800 USD,
+    const rate = new web3.BigNumber(9344); // ETH Price at 800 USD,
     const wallet = accounts[0];
-    const cap = web3.toWei(60000000, 'ether'); // Limit 50% of sale token in wei
-    const goal = web3.toWei(1, 'ether'); // No goal
+    const goal = ether(1);
+    const cap = ether(60000000);
 
-    if (network === "live") {
-        // Production we do not deploy new token.
-        return deployer.deploy(CarboneumCrowdsale, startTime, endTime, rate, wallet, cap, CarboneumToken.address, goal);
-    } else {
-        deployer.deploy(CarboneumToken).then(function () {
-            return deployer.deploy(CarboneumCrowdsale, startTime, endTime, rate, wallet, cap, CarboneumToken.address, goal);
-        });
-    }
+    let token, vault, crowdsale;
+    deployer.then(function () {
+        return CarboneumToken.new({from: wallet});
+    }).then(function (instance) {
+        token = instance;
+        return RefundVault.new(wallet, {from: wallet});
+    }).then(function (instance) {
+        vault = instance;
+        return CarboneumCrowdsale.new(startTime, endTime, rate, wallet, cap, token.address, goal);
+    }).then(function (instance) {
+        crowdsale = instance;
+        token.transferOwnership(crowdsale.address);
+        vault.transferOwnership(crowdsale.address);
+        console.log(crowdsale.address);
+        return true;
+    });
 };
--- readme.md
+++ readme.md
@@ -2,17 +2,7 @@
 
 ## Deploy
 ```
+$ npm install zeppelin-solidity
 $ truffle compile
 $ truffle migrate
-```
-
-## Try on truffle console 
-```
-account1 = web3.eth.accounts[1]
-CarboneumCrowdsale.deployed().then(inst => { crowdsale = inst })
-crowdsale.token().then(addr => { tokenAddress = addr } )
-tokenAddress
-caboneumTokenInstance = CarboneumToken.at(tokenAddress)
-CarboneumCrowdsale.deployed().then(inst => inst.sendTransaction({ from: account1, value: web3.toWei(5, "ether")}))
-caboneumTokenInstance.balanceOf(account1).then(balance => account1C8TokenBalance = balance.toString(10))
 ```
