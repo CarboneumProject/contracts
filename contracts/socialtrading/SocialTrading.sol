85bb6f53624203fc3dfda5338f9aadfc3f749679
artiya <artiya4u@gmail.com>
2018-08-16 15:49:27 +0700

    Update methods.

--- contracts/socialtrading/SocialTrading.sol
+++ contracts/socialtrading/SocialTrading.sol
@@ -1,45 +1,57 @@
 pragma solidity 0.4.24;
+pragma experimental ABIEncoderV2;
 
 import "zeppelin-solidity/contracts/ownership/Ownable.sol";
 import "zeppelin-solidity/contracts/token/ERC20/ERC20.sol";
 
 
-contract CopyTrading is Ownable {
+contract SocialTrading is Ownable {
   enum FeeType {
     REWARD_FEE,
-    RELAYER_FEE,
+    RELAY_FEE,
     VERIFIER_FEE
   }
   uint public rewardFee; //percentage times (1 ether)
-  uint public relayerFee; //percentage times (1 ether)
+  uint public relayFee; //percentage times (1 ether)
   uint public verifierFee; //percentage times (1 ether)
   ERC20 public feeToken;
   address public feeAccount;
 
   mapping(address => mapping(address => bool)) public following;
-  address[] public relayers;
-  address[] public verifiers;
+  mapping(address => uint) public relays;
+  mapping(address => uint) public verifiers;
+
+  mapping(address => uint256) rewards;
+  mapping(address => uint256) claimedRewards;
+
+  struct ClosePositionActivity {
+    bytes32 buyTx;
+    bytes32 sellTx;
+    address leaders;
+    address followers;
+    int256 gainLoss;
+  }
 
 
   function CopyTrading(
     address _feeAccount,
     ERC20 _feeToken,
     uint _rewardFee,
-    uint _relayerFee,
+    uint _relayFee,
     uint _verifierFee
   ) public
   {
     feeAccount = _feeAccount;
     rewardFee = _rewardFee;
     feeToken = _feeToken;
-    relayerFee = _relayerFee;
+    relayFee = _relayFee;
     verifierFee = _verifierFee;
   }
 
   event Follow(address leader, address follower);
   event UnFollow(address leader, address follower);
   event FeeChange(FeeType feeType, uint oldFee, uint newFee);
-  event AddRelayer(address relayer);
+  event AddRelay(address relay);
   event AddVerifier(address verifier);
   event Activities(bytes32 offChainHash);
 
@@ -52,9 +64,9 @@ contract CopyTrading is Ownable {
     rewardFee = _fee;
   }
 
-  function changeRelayerFee(uint _fee) external onlyOwner {
-    emit FeeChange(FeeType.RELAYER_FEE, relayerFee, _fee);
-    relayerFee = _fee;
+  function changeRelayFee(uint _fee) external onlyOwner {
+    emit FeeChange(FeeType.RELAY_FEE, relayFee, _fee);
+    relayFee = _fee;
   }
 
   function changeVerifierFee(uint _fee) external onlyOwner {
@@ -79,33 +91,42 @@ contract CopyTrading is Ownable {
   }
 
   /**
-   * @dev Register relayer to contract by the owner.
+   * @dev Register relay to contract by the owner.
    */
-  function registerRelayer(address relayer) onlyOwner external {
-    relayers.push(relayer);
-    emit AddRelayer(relayer);
+  function registerRelay(address relay) onlyOwner external {
+    relays[relay] = 1;
+    emit AddRelay(relay);
   }
 
   /**
    * @dev Register verifier to contract by the owner.
    */
   function registerVerifier(address verifier) onlyOwner external {
-    verifiers.push(verifier);
+    verifiers[verifier] = 1;
     emit AddVerifier(verifier);
   }
 
   /**
-   * @dev add trade activity log to contract by trusted relayer.
+   * @dev add trade activity log to contract by trusted relay.
    */
-  function tradeActivityBatch(bytes32 offChainHash) onlyRelay external {
-    emit Activities(offChainHash);
+  function tradeActivityBatch(bytes32 sideChainHash) external {
+    require(relays[msg.sender] == 1);
+    emit Activities(sideChainHash);
   }
 
   /**
-   * @dev Throws if called by any account other than the relayer.
+   * @dev add verify activity log result to contract by trusted verifier.
    */
-  modifier onlyRelay() {
-    require(msg.sender == owner);
-    _;
+  function verifyActivityBatch(ClosePositionActivity[] activity) external {
+    require(verifiers[msg.sender] == 1);
+    // TODO Calc reward.
+    // Transfer token from follower to this contract.
+    // Contract spiting fee to leader, relay and verifier the keep balance.
+  }
+
+  function claimReward() external {
+    claimedRewards[msg.sender] += rewards[msg.sender];
+    rewards[msg.sender] = 0;
+    require(feeToken.transfer(msg.sender, rewards[msg.sender]));
   }
 }
