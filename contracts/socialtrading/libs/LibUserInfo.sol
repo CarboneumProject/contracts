f92dc7553d4e7465433d286fde5b2cd9dc7a0eba
Artiya T <artiya4u@gmail.com>
2019-01-15 19:47:32 +0700

    Fix typo

--- contracts/socialtrading/libs/LibUserInfo.sol
+++ contracts/socialtrading/libs/LibUserInfo.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.4.24;
 contract LibUserInfo {
   struct Following {
     address leader;
-    uint percentage; // percentage times (100 = 100%)
+    uint percentage; // percentage (100 = 100%)
     uint index;
   }
 }
