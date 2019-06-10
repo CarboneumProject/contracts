f26a6950b0b3d77ba007ac12636471b118d46a2f
Artiya Thinkumpang <artiya4u@gmail.com>
2019-06-11 00:02:08 +0700

    Update promo code generator to use account sign on web3 1.0

--- GenPromoCode.js
+++ GenPromoCode.js
@@ -1,47 +1,58 @@
-const HDWalletProvider = require('truffle-hdwallet-provider');
 const Web3 = require('web3');
-const ContractABI = require('./build/contracts/CarboneumToken').abi;
+const { bufferToHex } = require('ethereumjs-util');
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const TokenABI = require('./build/contracts/CarboneumToken').abi;
+const PromoCodeABI = require('./build/contracts/PromoCode').abi;
 
 const providerWithMnemonic = (mnemonic, rpcEndpoint) =>
   new HDWalletProvider(mnemonic, rpcEndpoint);
 
 const infuraProvider = network => providerWithMnemonic(
   process.env.MNEMONIC || '',
-  `https://${network}.infura.io/${process.env.INFURA_API_KEY}`,
+  `https://${network}.infura.io/v3/96bfc78effaa4a32bf99ce0dd4453132`,
 );
-
-String.prototype.hashCode = function () {
-  let hash = 0, i, chr;
-  if (this.length === 0) return hash;
-  for (i = 0; i < this.length; i++) {
-    chr = this.charCodeAt(i);
-    hash = ((hash << 5) - hash) + chr;
-    hash |= 0; // Convert to 32bit integer
-  }
-  return hash;
-};
 
 const provider = infuraProvider(process.env.NETWORK || 'rinkeby');
 let w3 = new Web3(provider);
-console.log(provider.address);
-
-const amount = new w3.BigNumber('88e18');
+console.log('Promo Code Generator...');
+console.log('Owner Address:', provider.address);
+const amount = 88000000000000000000;
 const numberOfCode = 10;
-let i = 0;
-for (i; i < numberOfCode; i++) {
-  let code = 'C819' + Math.abs(Math.random() * 1000000).toFixed(0);
-  w3.eth.sign(provider.address, w3.fromUtf8(code), function (err, signature) {
-    if (!err) {
-      console.log(code, signature);
-    }
-  });
-}
+const approveAmount = amount * numberOfCode;
+const promocodeAddress = '0x5807d311d872e81709de391a8ad13f9e16c5443b';
+const tokenContractAddress = '0xd42debE4eDc92Bd5a3FBb4243e1ecCf6d63A4A5d';
 
-// Approve token
-const token = w3.eth.contract(ContractABI).at('0xd42debE4eDc92Bd5a3FBb4243e1ecCf6d63A4A5d');
-token.approve.sendTransaction('0x5807d311d872e81709de391a8ad13f9e16c5443b', amount.mul(i), { from: provider.address },
-  function (err, result) {
-    if (!err) {
-      console.log('Approve TX:', result);
-    }
-  });
+let i = 0;
+let code = '';
+let signature = '';
+let privateKey = bufferToHex(provider.wallet._privKey);
+console.log('==========================================  CODE  ==========================================');
+for (i; i <= numberOfCode; i++) {
+  code = 'C819' + Math.abs(Math.random() * 1000000).toFixed(0);
+  let sign = w3.eth.accounts.sign(w3.utils.fromUtf8(code), privateKey);
+  signature = sign.signature;
+  if (i < numberOfCode) {
+    console.log(code, signature);
+  }
+}
+console.log('============================================================================================');
+console.log('Approving contract spending...');
+const token = new w3.eth.Contract(TokenABI, tokenContractAddress);
+token.methods.approve(promocodeAddress, w3.utils.numberToHex(approveAmount)).send(
+  { from: provider.address, gas: 300000 }).on('transactionHash', (hash) => {
+  console.log('Approving Tx:', hash);
+  console.log('Testing Redeem Code...', code);
+  const promoCode = new w3.eth.Contract(PromoCodeABI, promocodeAddress);
+  promoCode.methods.redeem(code, signature).send(
+    { from: provider.address, gas: 300000 }).on('transactionHash', (redeemHash) => {
+    console.log('Test Redeem Tx:', redeemHash);
+    process.exit();
+  }).on('confirmation', (confirmationNumber, receipt) => {
+    console.log('confirmation2');
+  }).on('receipt', (receipt) => {
+  }).on('error', console.error);
+}).on('confirmation', (confirmationNumber, receipt) => {
+  console.log('confirmation1');
+}).on('receipt', (receipt) => {
+  console.log('receipt1');
+}).on('error', console.error);
