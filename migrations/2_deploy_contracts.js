df15f4de2749946bdfa404e6b0bb0ca328f0b898
Artiya Thinkumpang <artiya4u@gmail.com>
2019-09-04 16:17:04 +0700

    Change infura endpoint.

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,13 +1,14 @@
-const PromoCode = artifacts.require('PromoCode');
+const Purchase = artifacts.require('Purchase');
 const tokenAddress = '0xd42debe4edc92bd5a3fbb4243e1eccf6d63a4a5d';
+const fundWallet = '0x5023CfABAEa4d331fA78d50cc5de5102341df649';
 module.exports = function (deployer, network) {
   if (network === 'rinkeby') {
     return deployer.then(function () {
-      return PromoCode.new(tokenAddress, new web3.BigNumber('88e18'));
+      return Purchase.new(fundWallet, tokenAddress, new web3.BigNumber('555e18'));
     });
   } else {
     return deployer.then(function () {
-      return PromoCode.new(tokenAddress, new web3.BigNumber('88e18'));
+      return Purchase.new(fundWallet, tokenAddress, new web3.BigNumber('555e18'));
     });
   }
 };
